1a2394fad7f37b1a8211215fff0f2057e625b5c2
David Montgomery <davidmontgomery@gmail.com>
2026-03-28 01:49:02 +0800

    udpate

--- fix_agda.sh
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026 Daland Montgomery
-# SPDX-License-Identifier: AGPL-3.0-or-later
-
-#!/bin/bash
-set -e
-git checkout haskel/CrystalTopos.agda
-cat agda_bridge_proofs.agda >> haskel/CrystalTopos.agda
-echo "Done. Proofs: $(grep -c 'refl' haskel/CrystalTopos.agda)"
-echo "Now run: cd haskel && agda CrystalTopos.agda"
